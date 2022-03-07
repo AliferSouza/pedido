38553fa8a8c642e1fd4710ed5add4a983cb20693
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2022-03-07 17:28:41 -0300

    Agora terá cores no cardápio ...

--- src/pages/Cardapios.js
+++ src/pages/Cardapios.js
@@ -1,10 +1,20 @@
 import Button from "../componets/Button.js";
 import Produtos from "../componets/produtos.js";
 import Header from "../componets/Header.js";
-import Footer from "../componets/Footer.js";
+import Footer from "../componets/footer.js";
 
 
 export default function Caradapio(data, compararUrlEApi) {
+    let cardapioColor = []
+
+    compararUrlEApi.forEach((item) => {
+       cardapioColor.push(item.color)
+    })
+  
+
+ 
+
+
 
     let dadoslanche = data.filter((item) => {
         return (item.Categoria === "Lanche" && (item.Quantidade > 0))
@@ -21,34 +31,34 @@ export default function Caradapio(data, compararUrlEApi) {
     cardapio += `  
 
              <div class="container-header">
-             <div class="header">${Header(compararUrlEApi)}</div>
+             <div class="header" style="background : ${cardapioColor}">${Header(compararUrlEApi)}</div>
              </div>
      
              <div class="food-wrapper">
              <h4>Comida:</h4>
              <div class="foods">
-             ${Produtos(dadoslanche)}
+             ${Produtos(dadoslanche, cardapioColor)}
              </div>
              <br />
              
              <h4>Bebida:</h4>
              <div class="drinks">
-             ${Produtos(dadosBebida)}
+             ${Produtos(dadosBebida, cardapioColor)}
              </div>
              <br />
 
              <h4>Outros:</h4>
              <div class="outros">
-             ${Produtos(dadosOutros)}
+             ${Produtos(dadosOutros, cardapioColor)}
              </div>
              <br />
 
-             <div class="button">
+             <div class="button" style="background : ${cardapioColor}">
              ${Button()}
              </div>
              </div>
-             
-             ${Footer(compararUrlEApi)}          
+
+             ${Footer(compararUrlEApi, cardapioColor)}
       
   `;
 
