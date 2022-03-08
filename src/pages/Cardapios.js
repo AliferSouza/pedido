599c9e6b59582c5c11865a255daf480efac5a8e8
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2022-03-07 21:40:42 -0300

    Modificação footer

--- src/pages/Cardapios.js
+++ src/pages/Cardapios.js
@@ -1,7 +1,7 @@
 import Button from "../componets/Button.js";
 import Produtos from "../componets/produtos.js";
 import Header from "../componets/Header.js";
-import Footer from "../componets/footer.js";
+//import Footer from "../componets/footer.js";
 
 
 export default function Caradapio(data, cardapioColor, compararUrlEApi) {
@@ -50,7 +50,7 @@ export default function Caradapio(data, cardapioColor, compararUrlEApi) {
              </div>
              </div>
 
-             ${Footer(compararUrlEApi, cardapioColor)}
+             
 
           
       
